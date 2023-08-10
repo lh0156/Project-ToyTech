713877e4553ca29f8d93f1fa16806d4b1f64b115
yseom <lh0156@naver.com>
2023-08-10 10:26:19 +0900

    feat: init project

---
+++ src/main/java/kr/co/seop/toy/backtech/BacktechApplication.java
@@ -0,0 +1,13 @@
+package kr.co.seop.toy.backtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacktechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacktechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/seop/toy/backtech/BacktechApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.seop.toy.backtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
